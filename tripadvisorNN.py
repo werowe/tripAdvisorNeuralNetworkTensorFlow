077950e15ca8d5db0b4a74ae27bf13f1a353b910
werowe <werowe@walkerrowe.com>
2018-05-10 10:48:01 +0100

    Update tripadvisorNN.py

--- tripadvisorNN.py
+++ tripadvisorNN.py
@@ -12,47 +12,44 @@ FIELD_DEFAULTS = [[0], [0], [0], [0], [0],
 
 
 def parse_line(line):
-    try:
-        parsed_line = tf.decode_csv(line, FIELD_DEFAULTS)
-        tf.Print(input_=parsed_line[4], data=[parsed_line[4]], message="score")
-        label = parsed_line[4]
-        del parsed_line[4]
-        tf.Print(input_=parsed_line[1], data=[parsed_line[1]], message="Usercountry")
-        tf.Print(input_=parsed_line[2], data=[parsed_line[2]], message="Nrreviews")
-        tf.Print(input_=parsed_line[3], data=[parsed_line[3]], message="Nrhotelreviews")
-        tf.Print(input_=parsed_line[5], data=[parsed_line[5]], message="Helpfulvotes")
-        tf.Print(input_=parsed_line[6], data=[parsed_line[6]], message="Periodofstay")
-        tf.Print(input_=parsed_line[7], data=[parsed_line[7]], message="Travelertype")
-        tf.Print(input_=parsed_line[8], data=[parsed_line[8]], message="Pool")
-        tf.Print(input_=parsed_line[9], data=[parsed_line[9]], message="Gym")
-        tf.Print(input_=parsed_line[10], data=[parsed_line[10]], message="Tenniscourt")
-        tf.Print(input_=parsed_line[11], data=[parsed_line[11]], message="Spa")
-        tf.Print(input_=parsed_line[12], data=[parsed_line[12]], message="Casino")
-        tf.Print(input_=parsed_line[13], data=[parsed_line[13]], message="Freeinternet")
-        tf.Print(input_=parsed_line[14], data=[parsed_line[14]], message="Hotelname")
-        tf.Print(input_=parsed_line[15], data=[parsed_line[15]], message="Hotelstars")
-        tf.Print(input_=parsed_line[16], data=[parsed_line[16]], message="Nrrooms")
-        '''tf.Print(input_=parsed_line[17], data=[parsed_line[17]], message="Usercontinent")
-        tf.Print(input_=parsed_line[18], data=[parsed_line[18]], message="Memberyears")
-        tf.Print(input_=parsed_line[19], data=[parsed_line[19]], message="Reviewmonth")
-        tf.Print(input_=parsed_line[20], data=[parsed_line[20]], message="Reviewweekday")'''
-        features = parsed_line  
-        d = dict(zip(feature_names, features))
-        print ("dictionary", d, " label = ", label)    
-        return d, label
+    parsed_line = tf.decode_csv(line, FIELD_DEFAULTS)
+    tf.Print(input_=parsed_line , data=[parsed_line ], message="parsed_line ")
+    tf.Print(input_=parsed_line[4], data=[parsed_line[4]], message="score")
+    label = parsed_line[4]
+    del parsed_line[4]
+    tf.Print(input_=parsed_line[1], data=[parsed_line[1]], message="Usercountry")
+    tf.Print(input_=parsed_line[2], data=[parsed_line[2]], message="Nrreviews")
+    tf.Print(input_=parsed_line[3], data=[parsed_line[3]], message="Nrhotelreviews")
+    tf.Print(input_=parsed_line[5], data=[parsed_line[5]], message="Helpfulvotes")
+    tf.Print(input_=parsed_line[6], data=[parsed_line[6]], message="Periodofstay")
+    tf.Print(input_=parsed_line[7], data=[parsed_line[7]], message="Travelertype")
+    tf.Print(input_=parsed_line[8], data=[parsed_line[8]], message="Pool")
+    tf.Print(input_=parsed_line[9], data=[parsed_line[9]], message="Gym")
+    tf.Print(input_=parsed_line[10], data=[parsed_line[10]], message="Tenniscourt")
+    tf.Print(input_=parsed_line[11], data=[parsed_line[11]], message="Spa")
+    tf.Print(input_=parsed_line[12], data=[parsed_line[12]], message="Casino")
+    tf.Print(input_=parsed_line[13], data=[parsed_line[13]], message="Freeinternet")
+    tf.Print(input_=parsed_line[14], data=[parsed_line[14]], message="Hotelname")
+    tf.Print(input_=parsed_line[15], data=[parsed_line[15]], message="Hotelstars")
+    tf.Print(input_=parsed_line[16], data=[parsed_line[16]], message="Nrrooms")
+    '''tf.Print(input_=parsed_line[17], data=[parsed_line[17]], message="Usercontinent")
+    tf.Print(input_=parsed_line[18], data=[parsed_line[18]], message="Memberyears")
+    tf.Print(input_=parsed_line[19], data=[parsed_line[19]], message="Reviewmonth")
+    tf.Print(input_=parsed_line[20], data=[parsed_line[20]], message="Reviewweekday")'''
+    features = parsed_line  
+    d = dict(zip(feature_names, features))
+    tf.Print(input_=d, data=[d], message="dictionary")
+    print ("dictionary", d, " label = ", label)    
+    return d, label
     except InvalidArgumentError:
-        print ("InvalidArgumentError")
+    print ("InvalidArgumentError")
 
 
 def csv_input_fn(csv_path, batch_size):
-    try:
-        dataset = tf.data.TextLineDataset(csv_path)
-        dataset = dataset.map(parse_line)
-        dataset = dataset.shuffle(1000).repeat().batch(batch_size)
-        return dataset
-    except InvalidArgumentError:
-        print ("InvalidArgumentError")
-
+    dataset = tf.data.TextLineDataset(csv_path)
+    dataset = dataset.map(parse_line)
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    return dataset
 
 Usercountry = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Usercountry",47))
 
