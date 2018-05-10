aae2043a40b96c760d991967ddb5cee7798212ea
werowe <werowe@walkerrowe.com>
2018-05-10 11:52:24 +0100

    Update tripadvisorNN.py

--- tripadvisorNN.py
+++ tripadvisorNN.py
@@ -46,8 +46,6 @@ def csv_input_fn(csv_path, batch_size):
     dataset = dataset.shuffle(1000).repeat().batch(batch_size)
     return dataset
 
-
-
 Usercountry = tf.feature_column.numeric_column(("Usercountry",47))
 Nrreviews = tf.feature_column.numeric_column("Nrreviews")
 Nrhotelreviews = tf.feature_column.numeric_column("Nrhotelreviews")
@@ -67,32 +65,8 @@ Usercontinent = tf.feature_column.numeric_column(("Usercontinent",6))
 Memberyears = tf.feature_column.numeric_column("Memberyears")
 Reviewmonth = tf.feature_column.numeric_column(("Reviewmonth",12))
 Reviewweekday = tf.feature_column.numeric_column(("Reviewweekday",7))
-
-'''Usercountry = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Usercountry",47))
-Nrreviews = tf.feature_column.numeric_column("Nrreviews")
-Nrhotelreviews = tf.feature_column.numeric_column("Nrhotelreviews")
-Helpfulvotes = tf.feature_column.numeric_column("Helpfulvotes")
-Periodofstay = tf.feature_column.numeric_column("Periodofstay")
-Travelertype = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Travelertype",5))
-Pool = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Pool",2))
-Gym = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Gym",2))
-Tenniscourt = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Tenniscourt",2))
-Spa = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Spa",2))
-Casino = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Casino",2))
-Freeinternet = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Freeinternet",2))
-Hotelname = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Hotelname",22))
-Hotelstars = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Hotelstars",5))
-Nrrooms = tf.feature_column.numeric_column("Nrrooms")
-Usercontinent = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Usercontinent",6))
-Memberyears = tf.feature_column.numeric_column("Memberyears")
-Reviewmonth = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Reviewmonth",12))
-Reviewweekday = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_identity("Reviewweekday",7))
-'''
-
 feature_columns = [Usercountry, Nrreviews,Nrhotelreviews,Helpfulvotes,Periodofstay,
          Travelertype,Pool,Gym,Tenniscourt,Spa,Casino,Freeinternet,Hotelname,Hotelstars,Nrrooms,Usercontinent,Memberyears,Reviewmonth,Reviewweekday]
-
-
 classifier=tf.estimator.DNNClassifier(
     feature_columns=feature_columns,  
     hidden_units=[10, 10], 
